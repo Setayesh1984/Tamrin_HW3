be8ec9cd42966b63216954177d1f73910e0265dd
setayesh1984 <setayesh.asgari84@gmail.com>
2025-05-13 22:59:51 +0330

    Add Currency.hpp and cpp befor cHeck wit my friend

---
+++ include/currency.hpp
@@ -0,0 +1,43 @@
+#ifndef CURRENCY_HPP
+#define CURRENCY_HPP
+
+#include <iostream>
+
+class Currency 
+{
+
+
+protected:
+    // megdar pool bar asas arz
+    double value; 
+    // nesbat arz b dollar           
+    double conversionRate; 
+    
+    
+
+public:
+    Currency(double val = 0.0, double rate = 1.0);
+    
+    virtual ~Currency() = default;  
+
+    double getValue() const;
+    double getConversionRate() const;
+
+    void setValue(double val);
+    void setConversionRate(double rate);
+
+    // Operator overloads anche lazeme
+    Currency operator+(const Currency& other) const;
+    Currency operator-(const Currency& other) const;
+    Currency operator*(double factor) const;
+    Currency operator/(double divisor) const;
+
+    bool operator==(const Currency& other) const;
+    bool operator!=(const Currency& other) const;
+    bool operator<(const Currency& other) const;
+    bool operator>(const Currency& other) const;
+
+    virtual void print() const;  // chape arz
+};
+
+#endif
