58d26b844cb0ac3ec33cfd010afa85e2cdb28785
aylilin <aylinshahrokhi85@gmail.com>
2025-05-14 01:38:25 +0430

    Item.h added

---
+++ item_management/include/Item.h
@@ -0,0 +1,26 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <string>
+
+class Item
+{
+protected:
+    std::string name;
+    double price;
+    int quantity;
+    int stock;
+
+public:
+    Item(std::string name , double price , std::string unit , int stock);
+    virtual ~Item();
+
+    virtual void addUnit() = 0;
+    virtual void itemInfo() const = 0;
+
+    std::string getName() const;
+    double getPrice() const;
+    int getQuantity() const;
+    void reduceStock(double);
+};
+
+#endif
