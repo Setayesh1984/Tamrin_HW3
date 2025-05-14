74792ea478d15bae1c5b77a443cd89baae28f1d7
aylilin <aylinshahrokhi85@gmail.com>
2025-05-14 22:31:49 +0430

    function addUnit added

--- item_management/src/Seasoning.cpp
+++ item_management/src/Seasoning.cpp
@@ -3,3 +3,9 @@
 Seasoning::Seasoning(std::string name , double price , double quantity , std::string flavor) : Item(name , price , quantity) , flavor(flavor){}
 Seasoning::~Seasoning(){}
 
+void Seasoning::addUnit() 
+{
+    stock += 1;
+    std::cout << "one unit added to " << name << std::endl;
+    std::cout << "recent stock : " << stock << std::endl;
+}
