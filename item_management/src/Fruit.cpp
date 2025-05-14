72f7270d720525f70aa9f846aa650ad155823d1b
aylilin <aylinshahrokhi85@gmail.com>
2025-05-14 22:12:08 +0430

    function addUnit added

--- item_management/src/Fruit.cpp
+++ item_management/src/Fruit.cpp
@@ -3,3 +3,10 @@
 Fruit::Fruit(std::string name , double price , double quantity , std::string type) : Item(name , price , quantity) , type(type){}
 
 Fruit::~Fruit(){}
+
+void Fruit::addUnit()
+{
+    stock += 1;
+    std::cout << "one unit added to " << name << std::endl;
+    std::cout << "recent stock : " << stock << std::endl; 
+}
