9b03041242a569534772b6f3f67b3a634e583dd0
aylilin <aylinshahrokhi85@gmail.com>
2025-05-14 22:18:13 +0430

    stock az inja ham hazf shod

--- item_management/src/Item.cpp
+++ item_management/src/Item.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-Item::Item(string name , double price , double quantity , int stock) : name(name) , price(price) , quantity(quantity){}
+Item::Item(string name , double price , double quantity) : name(name) , price(price) , quantity(quantity){}
 Item::~Item(){}
 
 string Item::getName() const{
