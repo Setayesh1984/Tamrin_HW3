712efb6ab746afb287f6716db77ec50c17b4d6a7
setayesh1984 <setayesh.asgari84@gmail.com>
2025-05-15 15:55:38 +0330

    add class (just public)...>movaghat ta sakht main.cpp

--- include/bankaccount.hpp
+++ include/bankaccount.hpp
@@ -4,9 +4,17 @@
 
 class BankAccount 
 {
+    private:
+    Currency balance;                       //mojodi
+    double transferLimit;                   //max meghdari k mtnim bardarim
 
-    Currency balance;        //mojodi
-    double transferLimit;    //max meghdari k mtnim bardarim
+
+
+    public:
+    BankAccount(double limit = 1000.0);     //pishfarz max enteghal
+ 
+    void deposit(const Currency& amount);   //add money
+    bool withdraw(const Currency& amount);  //kasr money(bool)
 };
 
 #endif
