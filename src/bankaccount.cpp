a5c9c77ea72e8ece6676f9e6b5f0d75ac5e638f0
setayesh1984 <setayesh.asgari84@gmail.com>
2025-05-15 16:28:50 +0330

    add operator+ bara afzayesh mojodi

--- src/bankaccount.cpp
+++ src/bankaccount.cpp
@@ -4,3 +4,9 @@
 //constractor...mojodi:0...nerkh:1....pishfarz:1000
 BankAccount::BankAccount(double limit) : balance(0.0, 1.0), transferLimit(limit) {}
 
+//operator+ mojodi
+void BankAccount::deposit(const Currency& amount) 
+{
+    Currency converted(amount.getValue() * amount.getConversionRate(), 1.0);
+    balance = balance + converted;
+}
