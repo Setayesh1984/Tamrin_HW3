19184e5010362aec3172346cfadf26ffedbe1234
aylilin <aylinshahrokhi85@gmail.com>
2025-05-15 15:30:31 +0430

    function addUnit added

--- item_management/src/Snack.cpp
+++ item_management/src/Snack.cpp
@@ -3,3 +3,9 @@
 Snack::Snack(std::string name , double price , double quantity , int calories) : Item(name , price , quantity) , calories(calories){}
 Snack::~Snack(){}
 
+void Snack::addUnit() 
+{
+    stock += 1;
+    std::cout << "one snack added : " << name << std::endl;
+    std::cout << "recent stock : " << stock << std::endl;
+}
