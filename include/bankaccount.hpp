c8a65d429b5fa53fa15a1dc4d4eb2cd6e1205f49
setayesh1984 <setayesh.asgari84@gmail.com>
2025-05-15 15:40:22 +0330

    add class (just private)...>movaghat ta sakht main.cpp

---
+++ include/bankaccount.hpp
@@ -0,0 +1,12 @@
+#ifndef BANKACCOUNT_HPP
+#define BANKACCOUNT_HPP
+#include "currency.hpp"
+
+class BankAccount 
+{
+
+    Currency balance;        //mojodi
+    double transferLimit;    //max meghdari k mtnim bardarim
+};
+
+#endif
