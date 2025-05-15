a70bc85f50ac846ad982abbff812b7db04055712
setayesh1984 <setayesh.asgari84@gmail.com>
2025-05-15 16:14:44 +0330

    add constractor cpp...>movaghat ta sakht main.cpp

---
+++ src/bankaccount.cpp
@@ -0,0 +1,6 @@
+#include "bankaccount.hpp"
+#include <iostream>
+
+//constractor...mojodi:0...nerkh:1....pishfarz:1000
+BankAccount::BankAccount(double limit) : balance(0.0, 1.0), transferLimit(limit) {}
+
